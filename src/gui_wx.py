2f26cc431ca3d789851a037fe518f669d0110808
silicongarage <buster@silicongarage.org>
2024-10-04 00:52:18 -0500

    syntax cleanup

--- src/gui_wx.py
+++ src/gui_wx.py
@@ -403,7 +403,7 @@ class eqs(wx.Frame):
         dlg.ShowModal()
         dlg.Destroy()
 
-   def resource_path(self,relative_path):
+    def resource_path(self,relative_path):
         """ Get absolute path to resource, works for dev and for PyInstaller """
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
         return os.path.join(base_path, relative_path)
@@ -419,7 +419,7 @@ class eqs(wx.Frame):
         if os.path.exists(pth):
             thePath = pth
 
-       if thePath is not None:
+        if thePath is not None:
             provider = wx.SimpleHelpProvider()
             wx.HelpProvider.Set(provider)
 
